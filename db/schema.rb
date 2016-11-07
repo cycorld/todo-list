384a55eb883c264a576b0f18e30dc4195fa8265f
cycorld <cycorld.com@gmail.com>
2016-11-08 06:59:31 +0900

    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161107214803) do
+ActiveRecord::Schema.define(version: 20161107215840) do
 
   create_table "todos", force: :cascade do |t|
     t.string   "item",                       null: false
     t.boolean  "complete",   default: false
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_todos_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
