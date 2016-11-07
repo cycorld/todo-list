22e0fd96b0b0f6a7f2f3f17f2a23adc93ab6301a
cycorld <cycorld.com@gmail.com>
2016-11-08 07:13:48 +0900

    권한 before_action 걸기

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,5 +1,7 @@
 class TodosController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_todo, only: [:destroy, :edit, :update, :toggle]
+  before_action :check_owner, only: [:destroy, :edit, :update, :toggle]
 
   def index
     @todos = current_user.todos
@@ -11,25 +13,35 @@ class TodosController < ApplicationController
   end
 
   def destroy
-    todo = Todo.find(params[:id])
-    todo.destroy
+    @todo.destroy
     redirect_back fallback_location: root_path
   end
 
   def edit
-    @todo = Todo.find(params[:id])
   end
 
   def update
-    todo = Todo.find(params[:id])
-    todo.update(item: params[:item])
+    @todo.update(item: params[:item])
     redirect_to root_path
   end
 
   def toggle
     todo = Todo.find(params[:id])
-    todo.complete = !todo.complete
-    todo.save
+    @todo.complete = !todo.complete
+    @todo.save
     redirect_to root_path
+  end
+
+  private
+
+  def set_todo
+    @todo = Todo.find(params[:id])
+  end
+
+  def check_owner
+    unless current_user == @todo.user
+      flash[:alert] = '권한이 없습니다'
+      return redirect_to root_path
+    end
   end
 end
