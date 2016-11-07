8be3bfbc639cc767192f221c43663a257c4c41c4
cycorld <cycorld.com@gmail.com>
2016-11-08 00:35:58 +0900

    Todo의 complete의 기본값으로 false

--- db/migrate/20161107153502_create_todos.rb
+++ db/migrate/20161107153502_create_todos.rb
@@ -1,8 +1,8 @@
 class CreateTodos < ActiveRecord::Migration[5.0]
   def change
     create_table :todos do |t|
-      t.string :item
-      t.boolean :complete
+      t.string :item, null: false
+      t.boolean :complete, default: false
 
       t.timestamps
     end
