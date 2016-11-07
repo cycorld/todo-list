e79490e8ed17c493adcb4bf590a22cf9746e6eb5
cycorld <cycorld.com@gmail.com>
2016-11-08 01:07:04 +0900

    route 설정
    - root 'todos#index'
    - resoures :todos

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'todos/index'
+  root 'todos#index'
+  resources :todos
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
