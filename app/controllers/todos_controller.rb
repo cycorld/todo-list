e910d0d4e23b532d848dd3c2d66eccf84e04892c
cycorld <cycorld.com@gmail.com>
2016-11-08 01:29:09 +0900

    create 액션

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -2,4 +2,9 @@ class TodosController < ApplicationController
   def index
     @todos = Todo.all
   end
+
+  def create
+    Todo.create(item: params[:item], complete: false)
+    redirect_back fallback_location: root_path
+  end
 end
